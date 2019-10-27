285499e67186c135b4114b2eb8d6989b205c8f75
Jaiveer Singh <j.singh@berkeley.edu>
2019-10-26 20:17:20 -0700

    tea and boba dispensers online

--- chefBot/chefBotMain/chefBotMain.ino
+++ chefBot/chefBotMain/chefBotMain.ino
@@ -5,42 +5,80 @@
 // Create the motor shield object with the default I2C address
 Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 
-Servo pearlServo;
+const short CUP_SERVO_PIN = 9;
+const short PEARLS_SERVO_PIN = 10;
+
 Servo cupServo;
+Servo pearlsServo;
+
 Adafruit_DCMotor* teaPump = AFMS.getMotor(4);
 
 const String CMD_DISPENSE_CUP = "DispenseCup";
 const String CMD_DISPENSE_PEARLS = "DispensePearls";
 const String CMD_DISPENSE_TEA = "DispenseTea";
 
+const String DBG_READ_CUP = "ReadCup";
+const String DBG_READ_PEARLS = "ReadPearls";
+const String DBG_WRITE_CUP = "WriteCup";
+const String DBG_WRITE_PEARLS = "WritePearls";
+
 void dispenseCup() {
   Serial.println("Dispensing Cup");
 
-  cupServo.write(180);
-  delay(500);
   cupServo.write(0);
   delay(500);
+  
+  cupServo.write(120);
+  delay(250);
+  cupServo.write(100);
+  delay(250);
 
+  cupServo.write(120);
+  delay(250);
+  cupServo.write(100);
+  delay(250);
+
+  cupServo.write(120);
+  delay(250);
+  cupServo.write(100);
+  delay(250);
+
+  cupServo.write(0);
+  delay(250);
+  cupServo.write(40);
+  delay(250);
+  
   Serial.println("Cup Dispensed");
 }
+
+const short CLOSE_PEARLS = 40;
+const short OPEN_PEARLS = 90;
+const int HOLD_OPEN_PEARLS_DELAY = 5000;
+const int HOLD_CLOSE_PEARLS_DELAY = 250;
+const int NUM_PEARLS_SLICES = 5;
 
 void dispensePearls() {
   Serial.println("Dispensing Pearls");
 
-  pearlServo.write(180);
-  delay(500);
-  pearlServo.write(0);
+  for (int i = 0; i < NUM_PEARLS_SLICES; i++) {
+    pearlsServo.write(OPEN_PEARLS);
+    delay(HOLD_OPEN_PEARLS_DELAY);
+    pearlsServo.write(CLOSE_PEARLS);
+    delay(HOLD_CLOSE_PEARLS_DELAY);
+  }
+
   delay(500);
 
   Serial.println("Pearls Dispensed");
 }
 
+const int TEA_DISPENSE_TIME = 8000;
 void dispenseTea() {
   Serial.println("Dispensing Tea");
 
   teaPump->setSpeed(255);
   teaPump->run(FORWARD);
-  delay(500);
+  delay(TEA_DISPENSE_TIME);
   teaPump->run(RELEASE);
 
   Serial.println("Tea Dispensed");
@@ -49,12 +87,12 @@ void dispenseTea() {
 void setup() {
   Serial.begin(9600);           // set up Serial library at 9600 bps
 
-  pearlServo.attach(9);
-  cupServo.attach(10);
+  cupServo.attach(CUP_SERVO_PIN);
+  pearlsServo.attach(PEARLS_SERVO_PIN);
+
+  pearlsServo.write(CLOSE_PEARLS);
 
   AFMS.begin();  // create with the default frequency 1.6KHz
-
-  pearlServo.write(90);
 }
 
 void loop() {
@@ -67,6 +105,22 @@ void loop() {
       dispensePearls();
     } else if (cmd.equals(CMD_DISPENSE_TEA)) {
       dispenseTea();
+    } else if (cmd.equals(DBG_WRITE_CUP)) {
+      Serial.print("Cup servo target: ");
+      while (Serial.available() == 0) {}
+      long target = Serial.parseInt();
+      Serial.println(target);
+      cupServo.write(target);
+    } else if (cmd.equals(DBG_WRITE_PEARLS)) {
+      Serial.print("Pearls servo target: ");
+      while (Serial.available() == 0) {}
+      long target = Serial.parseInt();
+      Serial.println(target);
+      pearlsServo.write(target);
+    } else if (cmd.equals(DBG_READ_CUP)) {
+      Serial.println("Cup servo at " + cupServo.read());
+    } else if (cmd.equals(DBG_READ_PEARLS)) {
+      Serial.println("Pearls servo at " + pearlsServo.read());
     } else {
       Serial.println("Unknown command " + cmd);
     }
