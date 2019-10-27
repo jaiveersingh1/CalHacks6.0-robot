4bacef9b86c1b0903841ae3d036ce205296afe82
Jaiveer Singh <j.singh@berkeley.edu>
2019-10-26 22:42:38 -0700

    encoders in progress on waiterbot

--- waiterBot/waiterBotMain/waiterBotMain.ino
+++ waiterBot/waiterBotMain/waiterBotMain.ino
@@ -1,27 +1,72 @@
 #include <Wire.h>
-#include <Adafruit_MotorShield.h>
 
-// Create the motor shield object with the default I2C address
-Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
+const short LEFT_MOTOR_PIN = 6;
+const short RIGHT_MOTOR_PIN = 5;
 
-Adafruit_DCMotor* rbMotor = AFMS.getMotor(1);
-Adafruit_DCMotor* lbMotor = AFMS.getMotor(4);
+const String CMD_FORWARD = "forward";
+const String CMD_RIGHT_TURN = "right";
+const String CMD_LEFT_TURN = "left";
+
+const double LEFT_MOTOR_SCALE = 1.0;
+const double RIGHT_MOTOR_SCALE = 0.80;
+
+void drive(int leftPower, int rightPower) {
+  analogWrite(LEFT_MOTOR_PIN, (int) min(max(leftPower * LEFT_MOTOR_SCALE, 0), 255));
+  analogWrite(RIGHT_MOTOR_PIN, (int) min(max(rightPower * RIGHT_MOTOR_SCALE, 0), 255));
+}
+
+void forward(int duration = 500) {
+  drive(127, 127);
+
+  delay(duration);
+
+  drive(0, 0);
+}
+
+void rightTurn(int duration = 500) {
+  drive(127, 0);
+
+  delay(duration);
+
+  drive(0, 0);
+}
+
+void leftTurn(int duration = 500) {
+  drive(0, 127);
+
+  delay(duration);
+
+  drive(0, 0);
+}
 
 void setup() {
   Serial.begin(9600);           // set up Serial library at 9600 bps
 
-  AFMS.begin();  // create with the default frequency 1.6KHz
+  pinMode(LEFT_MOTOR_PIN, OUTPUT);
+  pinMode(RIGHT_MOTOR_PIN, OUTPUT);
 
-  rbMotor->setSpeed(75);
-  lbMotor->setSpeed(75);
-  rbMotor->run(FORWARD);
-  lbMotor->run(FORWARD);
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+
+  delay(3000);
+  digitalWrite(LED_BUILTIN, HIGH);
 
   delay(1000);
-  
-  rbMotor->run(RELEASE);
-  lbMotor->run(RELEASE);
+  forward(3000);
 }
 
 void loop() {
+  if (Serial.available() > 0) {
+    String cmd = Serial.readString();
+
+    if (cmd.equals(CMD_FORWARD)) {
+      forward();
+    } else if (cmd.equals(CMD_RIGHT_TURN)) {
+      rightTurn();
+    } else if (cmd.equals(CMD_LEFT_TURN)) {
+      leftTurn();
+    } else {
+      Serial.println("Unknown command " + cmd);
+    }
+  }
 }
